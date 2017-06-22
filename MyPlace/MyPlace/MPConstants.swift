08d0f4e6aa16990d8ddc529acd00021dfdaba86e
Santosh S <santosh.aigoo@gmail.com>
2017-06-22 19:00:37 +0800

    url

--- MyPlace/MyPlace/MPConstants.swift
+++ MyPlace/MyPlace/MPConstants.swift
@@ -17,6 +17,7 @@ let kGAPIKey = "AIzaSyBRXKxz7WL2-5iXecdh8-O9g3qO9JR1ohM"
 let kGMAPKey = "AIzaSyCVjeu5iooX5yUF4_T43mIiYPAI66gm8J0"
 
 let kGAPIPlace = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+let kGAPIPhoto = "https://maps.googleapis.com/maps/api/place/photo?" //maxwidth=400&photoreference=<Ref>&key=YOUR_API_KEY"
 
 let kMPLocationUpdateNotificationKey = "DidReceivedLocationUpdate"
 let kGMAPRadius = "5000" //5KM Radius
